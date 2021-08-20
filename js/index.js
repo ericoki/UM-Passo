eff566d4e5ad0a9a6fea8c053e3526d94be7756a
Eric Oki <eric.oki.ces@gmail.com>
2021-08-20 12:35:31 -0300

    .

--- js/index.js
+++ js/index.js
@@ -1,17 +1,18 @@
-let maxScroll = 0;
-let tagMenu = document.getElementById('menu');
-window.addEventListener('scroll', function (evnt) {
-    if (this.scrollY < maxScroll) {
-        tagMenu.style.top = this.scrollY + "px";
 
-        if (this.scrollY+100 < maxScroll) {
-            maxScroll = this.scrollY;
-        }
-    }
-    else {
-        maxScroll = this.scrollY;
-    }
-})
+//let maxScroll = 0;
+//let tagMenu = document.getElementById('menu');
+//window.addEventListener('scroll', function (evnt) {
+    //if (this.scrollY < maxScroll) {
+       // tagMenu.style.top = this.scrollY + "px";
+
+      //  if (this.scrollY+100 < maxScroll) {
+          //  maxScroll = this.scrollY;
+       // }
+   // }
+  //  else {
+     //   maxScroll = this.scrollY;
+  //  }
+//})
 
 //const hamburger = document.querySelector(".menu-toggle");
 //const navMenu = document.querySelector(".menu-section");
